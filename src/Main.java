ee36ae3a0b57cd1d893669dd5d3fe558ad887c2a
Vlad_Spirin <vlad.spirin1@yandex.ru>
2024-02-05 23:54:50 +0400

    Обозначаем переменные

--- src/Main.java
+++ src/Main.java
@@ -2,3 +2,35 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.PriorityQueue;
 import java.util.Scanner;
+
+public class Main {
+}
+class Toy implements Comparable<Toy> {
+    private int id;
+    private String name;
+    private double weight;
+
+    public Toy(int id, String name, double weight) {
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public int compareTo(Toy otherToy) {
+        // Сравниваем игрушки по весу для определения порядка в очереди
+        return Double.compare(this.weight, otherToy.weight);
+    }
+}
