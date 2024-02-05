4e54a16f6c62e833ae4637daa6130a12f27fedcf
Vlad_Spirin <vlad.spirin1@yandex.ru>
2024-02-05 23:59:15 +0400

    Пишем кейсы

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,39 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
+    private static final String RESULTS_FILE = "results.txt";
+    private static PriorityQueue<Toy> toyQueue = new PriorityQueue<>();
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+
+        while (isRunning) {
+            System.out.println("Select action:");
+            System.out.println("1. Add a new toy");
+            System.out.println("2. To hold a drawing");
+            System.out.println("3. Exit");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Очистка буфера
+
+            switch (choice) {
+                case 1:
+                    addNewToy(scanner);
+                    break;
+                case 2:
+                    Give();
+                    break;
+                case 3:
+                    isRunning = false;
+                    break;
+                default:
+                    System.out.println("Wrong choice, try again.");
+                    break;
+            }
+        }
+    }
 }
 class Toy implements Comparable<Toy> {
     private int id;
